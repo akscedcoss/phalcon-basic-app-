1c0d9fedad8e9810dd4300f12f7ae49769ecfcb7
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-03-28 10:54:48 +0530

    basic app using phalcon

---
+++ src/app/controllers/SignupController.php
@@ -0,0 +1,36 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class SignupController extends Controller
+{
+
+    public function IndexAction()
+    {
+    }
+
+    public function registerAction()
+    {
+        $user = new Users();
+        // print_r( $this->request->getPost()) ;
+        // die();
+        $user->assign(
+            $this->request->getPost(),
+            [
+                'name',
+                'email',
+                'password'
+            ]
+        );
+
+        $success = $user->save();
+
+        $this->view->success = $success;
+
+        if ($success) {
+            $this->view->message = "Register succesfully";
+        } else {
+            $this->view->message = "Not Register succesfully due to following reason: <br>" . implode("<br>", $user->getMessages());
+        }
+    }
+}
